4ac4d300346e586755701fe86619699bbf7b050b
Lafftale <lafftale1999@gmail.com>
2024-12-31 01:46:29 +0100

    its woooorking

--- main.cpp
+++ main.cpp
@@ -14,12 +14,16 @@ int main(void)
     int ci = 26;
     std::string firstName = bank.getClients()->getClients()[ci].getFirstName();
     std::string lastName = bank.getClients()->getClients()[ci].getLastName();
-    std::string account = bank.getClients()->getClients()[ci].getAccounts()[1];
+    // std::string account = bank.getClients()->getClients()[ci].getAccounts()[1];
 
-    std::cout << firstName << ' ' << lastName << ' ' << account << std::endl;
+    std::cout << firstName << ' ' << lastName << ' ' << std::endl;
 
-    std::cout << bank.getAccounts()->findAccount(account).getAccountNumber() << ' ';
-    std::cout << bank.getAccounts()->findAccount(account).getBalance() << "kr " <<std::endl;
+    for(auto account : bank.getClients()->getClients()[ci].getAccounts())
+    {
+        std::cout << bank.getAccounts()->findAccount(account).getAccountNumber() << ": ";
+        std::cout << bank.getAccounts()->findAccount(account).getBalance() << "kr " <<std::endl;
+    }
+    
     
     return 0;
 }
