55cf6a05c60534e3d109a4d62d42dc6394618f5b
Carl Broman <lafftale1999@gmail.com>
2025-01-04 12:37:55 +0100

    Pool is working

--- main.cpp
+++ main.cpp
@@ -1,50 +1,52 @@
 #include "include/generateClients.h"
-#include "include/bankQueue.h"
 #include "include/threadPool.h"
+#include "include/bankerUI.h"
 
 int main(void)
 {
-    ThreadPool pool(std::thread::hardware_concurrency());
 
-    BankQueue<int,10> queue;
+    GenerateClients gen(10);
 
-    // Add people to queue
-    pool.enqueue([&queue] {
-        while(queue.isRunning())
+    /* Bank bank(1000);
+
+    MenuNavigation layer = MAIN_MENU;
+
+    while(true)
+    {   
+        switch(layer)
         {
-            int rnd = rand() % 100;
-            queue.enqueue(rnd);
+            case MAIN_MENU:
+                layer = mainMenu();
+                continue;
 
-            std::cout << "Added " << rnd << " to queue" << std::endl;
+            case SERVE_CLIENT:
+                bank.setNextClient();
+                layer = serveClient(&bank);
+                continue;
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(250));            
+            case CHOOSE_CLIENT:
+                layer = chooseClient(&bank);
+                continue;
+
+            case NEW_CLIENT:
+                layer = newClient(&bank);
+                continue;
+
+            case CHECK_ACCOUNT:
+                layer = checkAccount(&bank);
+                continue;
+
+            case CREATE_ACCOUNT:
+                layer = createNewAccount(&bank);
+                continue;
+
+            case EXIT:
+                break;
         }
-    });
 
-    // Remove people from queue
-    pool.enqueue([&queue] {
-        while(queue.isRunning())
-        {
-            auto item = queue.dequeue();
-            if(item)
-            {
-                std::cout << "Removed " << *item << " from queue" << std::endl;
-            }
-            else
-            {
-                std::cout << "Failed to add to queue" << std::endl;
-            }
-
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    });
-
-    // Condition to finish queue
-    pool.enqueue([&queue] {
-        std::this_thread::sleep_for(std::chrono::seconds(20));
-        queue.stopQueue();
-        std::cout << "Queue was stopped" << std::endl;
-    });
-
+        break;
+    }
+ */
     return 0;
+
 }
